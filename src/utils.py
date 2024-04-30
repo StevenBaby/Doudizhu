dac9e87d469d5845de226045e8d2b254f71986d3
Steven Kang <kangweibaby@163.com>
2024-04-30 22:10:36 +0800

    add capture for application

---
+++ src/utils.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import collections
+
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+
+def imshow(img: np.ndarray, figsize=(12, 2)):
+    if img is None:
+        return
+    if figsize:
+        fig = plt.figure(figsize=figsize)
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+    plt.close()
+
+
+def imshows(imgs: list[np.ndarray], ):
+    if not imgs:
+        return
+    fig = plt.figure(figsize=(len(imgs), 2))
+    axes = fig.subplots(1, ncols=len(imgs))
+
+    for idx, img in enumerate(imgs):
+        if len(imgs) > 1:
+            axes[idx].imshow(img)
+            axes[idx].axis("off")
+            axes[idx].text(5, 5, idx)
+        else:
+            axes.imshow(img)
+            axes.axis("off")
+            axes.text(5, 5, idx)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # cv2.imwrite(f"qq/images/{idx}.png", img)
+    plt.show()
